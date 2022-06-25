d6ef739084e520760eba2d496de9b1ffa0bdf653
MoskalykA <streamarthur462@gmail.com>
2022-06-25 09:55:47 +0200

    feat: add index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,17 @@
+import { TbMathSymbols, TbWriting } from "react-icons/tb"
+
 function Index() {
    return (
-      <div>
-         
+      <div className="flex justify-center items-center h-screen">
+         <div className="flex space-x-2">
+            <div className="flex justify-center items-center border border-zinc-700 rounded p-4 transition-all duration-1000 hover:shadow-2xl hover:cursor-pointer hover:p-8">
+               <TbMathSymbols className="w-5 h-5 text-white"/>
+            </div>
+
+            <div className="flex justify-center items-center border border-zinc-700 rounded p-4 transition-all duration-1000 hover:shadow-2xl hover:cursor-pointer hover:p-8">
+               <TbWriting className="w-5 h-5 text-white"/>
+            </div>
+         </div>
       </div>
    )
 }
